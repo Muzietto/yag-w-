12d281c55acd122038bbc15216604793a82e639c
Muzietto <marco_faustinelli@yahoo.com>
2016-01-10 14:53:00 +0100

    green bars - bye bye demo!

--- mytodo/test/spec/controllers/main.js
+++ mytodo/test/spec/controllers/main.js
@@ -17,19 +17,19 @@ describe('Controller: MainCtrl', function () {
   }));
 
   it('should attach a list of todos to the scope', function () {
-    expect(scope.todos.length).toBe(4);
+    expect(scope.todos.length).toBe(0);
   });
   
   it('should add items to the list', function () {
   scope.todo = 'Test 1';
   scope.addTodo();
-  expect(scope.todos.length).toBe(5);
+  expect(scope.todos.length).toBe(1);
   });
 
   it('should add then remove an item from the list', function () {
     scope.todo = 'Test 12';
     scope.addTodo();
-    scope.removeTodo(6);
-    expect(scope.todos.length).toBe(5);
+    scope.removeTodo(2);
+    expect(scope.todos.length).toBe(1);
   });
 });
