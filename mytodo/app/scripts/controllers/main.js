9712fe03a136685f14adc1c0464047a31faefcc9
Muzietto <marco_faustinelli@yahoo.com>
2016-01-10 10:39:43 +0100

    eventually following the demo

--- mytodo/app/scripts/controllers/main.js
+++ mytodo/app/scripts/controllers/main.js
@@ -17,6 +17,23 @@ angular.module('mytodoApp')
       'Karrma',
       'Livestrong'
     ];
+    
+    $scope.addTodo = function() {
+      $scope.todos.push($scope.todo);
+      $scope.todo = '';
+    };
+    
+    $scope.removeTodo = function(index) {
+      $scope.todos.splice(index, 1);
+    };
+    
+    $scope.removeFirst = function() {
+      $scope.todos.splice(0, 1);
+    };
+    
+    $scope.removeLast = function() {
+      $scope.todos.splice($scope.todos.length - 1, 1);
+    };
   });
 
 }());
