a0b3bde5a210ce26ab68bbbc0a5c8fbda3d71cef
Muzietto <marco_faustinelli@yahoo.com>
2016-01-10 11:09:13 +0100

    kinda testing

--- mytodo/test/spec/controllers/main.js
+++ mytodo/test/spec/controllers/main.js
@@ -16,7 +16,20 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should attach a list of todos to the scope', function () {
+    expect(scope.todos.length).toBe(4);
   });
+  
+  it('should add items to the list', function () {
+  scope.todo = 'Test 1';
+  scope.addTodo();
+  expect(scope.todos.length).toBe(5);
+});
+
+it('should add then remove an item from the list', function () {
+  scope.todo = 'Test 12';
+  scope.addTodo();
+  scope.removeTodo(6);
+  expect(scope.todos.length).toBe(5);
+});
 });
